3b0ca8fbb753f49d91c41445a5e4b406a73a53cf
Victor Muhita <wangombevictor5@gmail.com>
2025-07-04 13:51:40 +0300

    add join Queries

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,47 @@
+-- INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    bookings.*,
+    users.*
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.id;
+
+-- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    properties.*,
+    reviews.*
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id;
+
+-- FULL OUTER JOIN (simulated using UNION): Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT 
+    users.*, 
+    bookings.*
+FROM 
+    users
+LEFT JOIN 
+    bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.*, 
+    bookings.*
+FROM 
+    users
+RIGHT JOIN 
+    bookings ON users.id = bookings.user_id;
+
+
+SELECT users.*, bookings.* 
+FROM users 
+LEFT JOIN bookings ON users.id = bookings.user_id
+
+UNION
+
+SELECT users.*, bookings.* 
+FROM users 
+RIGHT JOIN bookings ON users.id = bookings.user_id;
